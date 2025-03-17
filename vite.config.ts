9d9b59be988025a50899b678e403e1fc5004f4e5
EgonTehno <76494607+EgonTehno@users.noreply.github.com>
2025-03-17 13:12:23 +0200

    Update vite.config.ts
    tehnosysteemid.ee > proov10

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/tehnosysteemid.ee/', // Make sure this matches your repo name
+  base: '/proov10/', // Make sure this matches your repo name
   plugins: [react()],
   build: {
     minify: true,
